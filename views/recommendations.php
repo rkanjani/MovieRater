cb4225ed0b20fbc827a4642678fc1bef03fcc837
Rohan <rkanj017@uottawa.ca>
2016-04-11 15:19:02 -0400

    everything is fixed

--- views/recommendations.php
+++ views/recommendations.php
@@ -58,7 +58,6 @@
 <?php
     //runs the recommend query and gets a value for each genre watched
     while($the_query_row=pg_fetch_row($the_query_res)):
-
   //selects the tags from the above query
     $tag_query="SELECT name, tag_id FROM movie_rater.tag WHERE tag_id = '$the_query_row[0]'
   ORDER BY name ASC;";
@@ -82,9 +81,10 @@
      movie_rater.tag t, movie_rater.movie_tags mt, movie_rater.watches w WHERE t.tag_id = '$tag_row[1]'
       AND t.tag_id = mt.tag_id AND mt.movie_id =  m.movie_id AND NOT m.movie_id = w.movie_id ORDER BY w.rating DESC;";
       $movie_res=pg_query($dbconn,$movie_query);
-    if(!$movie_res){
-      die("Error in SQL query: " .pg_last_error());
-    }
+      if(!$movie_res){
+        die("Error in SQL query: " .pg_last_error());
+      }
+    
       ?>
 
     	<div class="row-fluid">
@@ -104,17 +104,14 @@
 
 
             ?>
-          <div class="movie-holder">
+          <div class="movie-holder-recc">
 
-                <a href="#">
-                  <span class="movie">
+                <a href="<?="#"."popup".$movie_row[2] ?>">
+                  <span class="movie-recc">
                     <img class="completed" src="<?php echo "../img/".$movie_row[0].".jpg"?>" width=148 height=215>
                     <h4 class="movie-title"><?php $pieces=explode("-", $movie_row[1]); echo $movie_row[0]." (".$pieces[0].")" ?> </h4>
                   </span>
                 </a>
-              <div id="" class="overlay">
-                  <div class="popup">
-                      <h2><?php echo $movie_row[0] ?></h2>
 
                       <?php
                       //selects required information for directors
@@ -125,49 +122,103 @@
                           if(!$res2){
                             die("Error in SQL query: " .pg_last_error());
                           }
+
+                          $director_query="SELECT first_name, last_name FROM movie_rater.director d, 
+                          movie_rater.directs ds, movie_rater.movie m WHERE d.director_id=ds.director_id
+                          AND m.movie_id='$movie_row[2]'AND ds.movie_id=m.movie_id;";
+                           $res2=pg_query($dbconn,$director_query);
+                          if(!$res2){
+                            die("Error in SQL query: " .pg_last_error());
+                          }
+
+                          $actor_query="SELECT first_name, last_name FROM movie_rater.actor a,
+                          movie_rater.actor_plays ap, movie_rater.movie m WHERE m.movie_id = '$movie_row[2]' AND
+                          m.movie_id=ap.movie_id AND ap.actor_id=a.actor_id;";
+                          $res3=pg_query($dbconn,$actor_query);
+                          if(!$res3){
+                            die("Error in SQL query: " .pg_last_error());
+                          }
+
+                          $studio_query="SELECT name FROM movie_rater.studio s, movie_rater.sponsors sp,
+                          movie_rater.movie m WHERE m.movie_id='$movie_row[2]' AND m.movie_id = sp.movie_id AND
+                          sp.studio_id = s.studio_id;";
+                          $res4=pg_query($dbconn,$studio_query);
+                          if(!$res4){
+                            die("Error in SQL query: " .pg_last_error());
+                          }
+
+                          $topic_query="SELECT description FROM movie_rater.topics t, movie_rater.movie_topics mt,
+                          movie_rater.movie m WHERE m.movie_id='$movie_row[2]' AND m.movie_id = mt.movie_id AND
+                          mt.topic_id = t.topic_id;";
+                          $res5=pg_query($dbconn,$topic_query);
+                          if(!$res5){
+                            die("Error in SQL query: " .pg_last_error());
+                          }
+
+                          $watched_query="SELECT rating FROM movie_rater.watches WHERE movie_id='$movie_row[2]' AND user_id= '$user'; ";
+                          $watchedres=pg_query($dbconn,$watched_query);
+                          if(!$watchedres){
+                            die("Error in SQL query: " .pg_last_error());
+                          }
+                          $flag=true;
+
+                          if(pg_num_rows($watchedres)==1){
+                            $image = "../img/stamp.png";
+                          }
+                          else{
+                            $image = "../img/transparent.png";
+                          }
                       ?>
+
+                      <div id="<?= "popup".$movie_row[2] ?>" class="overlay">
+                                <div id="popup-recc" class="popup">
+                              <h2><?php $pieces=explode("-", $movie_row[1]); echo $movie_row[0]." (".$pieces[0].")" ?></h2>
 
                       <a class="close" href="#">&times;</a>
                       <div class="content">
-                          <img src="" height=250 style="float:left;"></img>
+                          <img src="<?php echo "../img/".$movie_row[0].".jpg"?>" height=250 style="float:left;">
                           <div class="movie-info">
 
-                              <p class="directors">
-                                <b>Director(s):</b>
-                                  
-                              </p>
+                            <p class="directors">
+                             <b>Director(s):</b>
+                              <?php while ($row2 = pg_fetch_row($res2)): ?>
+                                <p class="directors"><?php echo $row2[0]." ".$row2[1]; ?></p>
+                              <?php endwhile ?>
+
+                            </p>
 
 
-                              <p class="actors">
+                            <p class="actors">
                               <b>Starring: </b>
+                              <?php while ($row3 = pg_fetch_row($res3)): ?>
+                              <p class="actors"><?php echo $row3[0]." ".$row3[1]; ?> </p>
+                            <?php endwhile ?>
 
-                              <p class="actors">Actors</p>
+                            </p>
 
-
-                              </p>
-
-                              <p class="studio">
+                            <p class="studio">
                               <b>Produced by: </b>
-                              <p class="studio">Studio</p>
+                              <?php while ($row4 = pg_fetch_row($res4)): ?>
+                              <p class="studio"><?php echo $row4[0]; ?> </p>
+                            <?php endwhile ?>
+                            </p>
 
-                              </p>
-
-                              <p class="movie-description">
+                            <p class="movie-description">
                               <b>Synopsis: </b>
-                              <p class="movie-description">Synopsis</p>
-                              </p>
+                              <?php while ($row5 = pg_fetch_row($res5)): ?>
+                              <p class="movie-description"><?php echo $row5[0]; ?> </p>
+                            <?php endwhile ?>
+                            </p>
                           </div>
-                          <a name="trailer" href="https://www.youtube.com/watch?v=Deadpool" target="_blank" value="Trailer" class="btn btn-default trailer">Trailer</a>
-                      </div>
 
-                  </div>
-            </div>
-         
+                            <a name="trailer" href="<?php echo $row[3] ?>" target="_blank" value="Trailer" class="btn btn-default trailer">Trailer</a>
+                        </div>
+              </div>
           </div>
-           <?php endwhile ?>
+           
 
       </div>
-
+<?php endwhile ?>
 
 
     </div>
